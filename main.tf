f0012129984e6b79b775d10abc201be793e64159
John Gruber <jgruber@f5.com>
2021-05-13 08:05:45 -0500

    removed VPC generation attribute

--- main.tf
+++ main.tf
@@ -11,9 +11,6 @@ terraform {
   }
 }
 provider "ibm" {
-  /* Uncomment ibmcloud_api_key while testing from CLI */
-  // ibmcloud_api_key = var.api_key
-  generation       = 2
   region           = var.region
   ibmcloud_timeout = 300
 }
